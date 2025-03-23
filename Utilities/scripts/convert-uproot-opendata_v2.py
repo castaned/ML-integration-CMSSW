1629626e81916ab4ccf6c92a1333135891fc32a5
Alfredo Castaneda <castaned@cern.ch>
2025-03-23 23:10:46 +0100

    adding variables in convert-uproot-opendata_v2.py

--- Utilities/scripts/convert-uproot-opendata_v2.py
+++ Utilities/scripts/convert-uproot-opendata_v2.py
@@ -20,6 +20,18 @@ tree = upfile['deepntuplizer/Events']
 
 # Define branch groups
 other_branches = ['event']
+other_branches = ['Dataset_ID']
+other_branches = ['MET_pt']
+other_branches = ['nElectron']
+other_branches = ['nMuon']
+other_branches = ['A_Zmass']
+other_branches = ['B_Zmass']
+other_branches = ['C_Zmass']
+other_branches = ['D_Zmass']
+other_branches = ['A_Dr_Z']
+other_branches = ['B_Dr_Z']
+other_branches = ['C_Dr_Z']
+other_branches = ['D_Dr_Z']
 electron_branches = []
 muon_branches = []
 
