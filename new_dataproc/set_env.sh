5d518ac628ed9e39cbeac51050af97bc8d474215
Victor Manuel Minjares Neriz <91238149+victor-MMN@users.noreply.github.com>
2025-10-29 21:30:44 -0700

    Fix syntax for variable expansion in set_env.sh

--- new_dataproc/set_env.sh
+++ new_dataproc/set_env.sh
@@ -3,7 +3,7 @@
 echo "Starting job on $(date)"
 echo "Running on: $(hostname)"
 
-mkdir -p $EOS_OUTPUT_DIR
-tar -zcf "$EOS_OUTPUT_DIR/CMSSW.tgz" $AFS_CMS_BASE
+mkdir -p "$EOS_OUTPUT_DIR"
+tar -zcf "$EOS_OUTPUT_DIR/CMSSW.tgz" "$AFS_CMS_BASE"
 
 echo "Time $(date)"
