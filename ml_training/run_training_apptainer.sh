67836e98d13c807085f0b2f45b4236ed6be36fea
Victor Manuel Minjares Neriz <vminjare@lxplus970.cern.ch>
2025-11-28 08:25:24 +0100

    Update: Add time in .sh file and the HTCondor logs in logs dir

--- ml_training/run_training_apptainer.sh
+++ ml_training/run_training_apptainer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "Starting job on $(date)"
+echo "Running on: $(hostname)"
+
 apptainer run --bind /eos --bind $PWD:/workspace tool_container.sif /workspace/main.py -f /workspace/ml_model_config.yaml
 
 xrdfs "${REDIRECTOR}" mkdir -p "${EOS_DIR}"
 xrdcp -r "${RESULTS_DIR}" "${REDIRECTOR}/${EOS_DIR}/"
+
+echo "Time $(date)"
