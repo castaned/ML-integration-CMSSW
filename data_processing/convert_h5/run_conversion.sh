f19d56f5d936e383b45310dc5258afe1fdc89680
Victor Manuel Minjares Neriz <vminjare@lxplus970.cern.ch>
2025-11-28 09:02:37 +0100

    Update: Remove my directories from config files

--- data_processing/convert_h5/run_conversion.sh
+++ data_processing/convert_h5/run_conversion.sh
@@ -6,9 +6,6 @@ echo "Running on: $(hostname)"
 INPUT_FILE=$1
 OUTPUT_FILE=$2
 
-#xrdcp "root://eosuser.cern.ch/${INPUT_FILE}" .
-ls
-
 apptainer exec --bind /eos conversion_container.sif python3 - <<EOF
 import utilities.root as root
 
@@ -20,6 +17,5 @@ max_jagged_len = int("$MAX_JAGGED_LEN")
 
 root.root_to_h5(input_path, tree, branches, output_path, max_len=max_jagged_len)
 EOF
-
 
 echo "Time $(date)"
